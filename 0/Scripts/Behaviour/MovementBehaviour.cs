38d4ef680b32648d486ee1d8eefec21a10a518e7
Lindiao <kclo9501@gmail.com>
2017-10-25 02:44:35 +0800

    Add files via upload

--- 0/Scripts/Behaviour/MovementBehaviour.cs
+++ 0/Scripts/Behaviour/MovementBehaviour.cs
@@ -8,11 +8,13 @@ public class MovementBehaviour : StateMachineBehaviour
         anim.ResetTrigger("Skill");
         anim.SetBool("Shot", false);
         anim.GetComponentInParent<Controller>().lockMove = false ;
+        anim.SetFloat("Move", Mathf.Abs(0));
     }
 
     override public void OnStateUpdate(Animator anim, AnimatorStateInfo info, int id)
     {
         Game.sav.RegenSP(Time.deltaTime);
+        Game.sav.RegenHP(Time.deltaTime);
         anim.GetComponentInParent<Controller>().lockMove = false;
     }
 }
