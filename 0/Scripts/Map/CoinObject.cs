e29dfbb11ed021de2299de432670f9d51d302488
Lindiao <kclo9501@gmail.com>
2017-10-25 02:42:56 +0800

    Add files via upload

--- 0/Scripts/Map/CoinObject.cs
+++ 0/Scripts/Map/CoinObject.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class CoinObject : MonoBehaviour {
     public int amount = 1;
+    public World world;//音效
+    public AudioClip moveSound;//音效
+
+    //音效
+    void Start()
+    {
+        world = FindObjectOfType<World>();
+    }
 
     void OnTriggerEnter2D(Collider2D co)
     {
@@ -11,5 +19,6 @@ public class CoinObject : MonoBehaviour {
         Game.sav.GainMoney(1);
         GetComponent<Animator>().SetTrigger("Eat");
         Destroy(this);
+        world.sound.PlaySE(moveSound);//音效
     }
 }
---
+++ 0/Scripts/Map/Reset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D co)
+    {
+        if (co.gameObject.tag == "Player" && co.contacts[0].normal == -Vector2.up)
+        {
+            // Die
+            gameObject.layer = 0;
+            this.enabled = false;
+            StartCoroutine(Reload());
+        }
+    }
+
+    public IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(3);
+        Game.screen().FadeAndGo(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        Game.Reset();
+    }
+}
